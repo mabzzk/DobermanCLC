8a96fa862cd9cb673135cde6b29ea2cb4a8d5de1
mabzzk <kittelsen.andreas@gmail.com>
2022-03-03 21:00:54 +0100

    textFit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
 //Circle stuff
-let radius = 150;
+let radius = 155;
 new CircleType(document.getElementById('text')).radius(radius)
 new CircleType(document.getElementById('text2')).radius(radius)
 new CircleType(document.getElementById('text3')).radius(radius)
