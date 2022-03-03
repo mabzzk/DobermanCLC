6b4daa73f321afda8427f9e0a345ab44c8fe6c4c
mabzzk <kittelsen.andreas@gmail.com>
2022-03-04 00:31:44 +0100

    div

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 
-
 //Circle stuff
-let radius = 155;
-new CircleType(document.getElementById('text')).radius(radius)
-new CircleType(document.getElementById('text2')).radius(radius)
-new CircleType(document.getElementById('text3')).radius(radius)
+
 
 
 // CURSOR STUFF
---
+++ js/spinning.js
@@ -0,0 +1,5 @@
+//Circle stuff
+let radius = 145;
+new CircleType(document.getElementById('text')).radius(radius)
+new CircleType(document.getElementById('text2')).radius(radius)
+new CircleType(document.getElementById('text3')).radius(radius)
