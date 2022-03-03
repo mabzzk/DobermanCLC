1e098c0a6fbd95b50f63b31111bb7b99c2a369b1
mabzzk <kittelsen.andreas@gmail.com>
2022-03-03 19:04:08 +0100

    bigbig

--- js/dobermanThing.js
+++ js/dobermanThing.js
@@ -6,11 +6,8 @@ import { fragmentShader } from "./fragment.js";
 
 
 
-var cursor = document.getElementById("cursor");
-document.body.addEventListener("mousemove", function(e) {
-  cursor.style.left = e.clientX + "px",
-    cursor.style.top = e.clientY + "px";
-});
+
+
 
 
 
