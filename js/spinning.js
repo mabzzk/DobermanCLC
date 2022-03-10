c49bfcf98388c4cbf0027548a18cfa2a988ad218
mabzzk <kittelsen.andreas@gmail.com>
2022-03-10 11:39:22 +0100

    added extra page

--- js/spinning.js
+++ js/spinning.js
@@ -1,5 +1,3 @@
 //Circle stuff
 let radius = 145;
-new CircleType(document.getElementById('text')).radius(radius)
-new CircleType(document.getElementById('text2')).radius(radius)
-new CircleType(document.getElementById('text3')).radius(radius)
+new CircleType(document.getElementById('textNew')).radius(radius)
